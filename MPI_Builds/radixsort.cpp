8a999b12092e0843b3b8ded343de67cdc367b9ed
JerryTran03 <114455196+JerryTran03@users.noreply.github.com>
2024-10-22 01:27:46 -0500

    Update radixsort.cpp

--- MPI_Builds/radixsort.cpp
+++ MPI_Builds/radixsort.cpp
@@ -1,12 +1,10 @@
 #include "mpi.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
-#include <iostream>
 #include <vector>
 #include <algorithm>
-#include <cmath>
-#include <caliper/cali.h> 
+#include <cstring>
+#include <caliper/cali.h>
 #include <caliper/cali-manager.h>
 #include <adiak.hpp>
 
@@ -25,194 +23,183 @@ const char* comp = "comp";
 const char* comp_small = "comp_small";
 const char* comp_large = "comp_large";
 
-int* GenerateArray(int size, int inputType, int processorNumber, int totalProcesses) {
+int* GenerateArray(int size, int inputType, int processorNumber) {
     int* arr = new int[size];
-
-    // Seed the random number generator for better randomness
-    srand(static_cast<unsigned int>(rand()) + processorNumber); 
+    srand(static_cast<unsigned int>(rand()) + processorNumber * 100);
 
     switch (inputType) {
         case RANDOM:
-            // Generate random integers between 0 and 9999
             for (int i = 0; i < size; i++) {
                 arr[i] = rand() % 10000;
             }
             break;
-
         case SORTED:
-            // Generate sorted integers with an offset for processor ID
             for (int i = 0; i < size; i++) {
-                arr[i] = i + (size * processorNumber); // Ensures distinct sorted arrays per processor
+                arr[i] = i + processorNumber * size;
             }
             break;
-
         case REVERSE_SORTED:
-            // Generate reverse sorted integers with an offset for processor ID
-            for (int i = 0; i < size; i++) {
-                arr[i] = (size - i - 1) + (size * processorNumber); // Distinct reverse sorted arrays
+            for (int i = size - 1; i >= 0; i--) {
+                arr[i] = (size - i - 1) + processorNumber * size;
             }
             break;
-
         case NOISE:
-            // Create a sorted array first
             for (int i = 0; i < size; i++) {
-                arr[i] = i + (size * processorNumber); 
+                arr[i] = i + processorNumber * size;
             }
-
-            // Randomly update 1% of the elements
-            for (int i = 0; i < size / 100; ++i) { 
-                int index = rand() % size;
-                arr[index] = rand() % 10000; // Random value
+            for (int i = 0; i < size / 100; i++) {
+                arr[rand() % size] = rand() % 10000;
             }
             break;
-
-        default:
-            // Invalid input type; handle error
-            printf("Invalid input type specified.\n");
-            delete[] arr; // Clean up before returning
-            return nullptr;
     }
 
     return arr;
 }
 
 void PrintArray(int* arr, int size, int processorNumber) {
-    printf("Processor number %d, array:", processorNumber);
+    printf("Processor %d, array: ", processorNumber);
     for (int i = 0; i < size; i++) {
         printf("%d ", arr[i]);
     }
     printf("\n");
 }
 
-bool SortedVerification(int* arr, int size) { // Check if an array was sorted correctly
-    int max = 0;
-    for (int i = 0; i < size; i++) {
-        if (arr[i] < max) {
+bool SortedVerification(int* arr, int size) {
+    for (int i = 1; i < size; i++) {
+        if (arr[i - 1] > arr[i]) {
             return false;
         }
-        max = arr[i];
     }
     return true;
 }
 
-// Radix Sort Function
-void radixSort(std::vector<int>& arr) {
-    int max_element = *std::max_element(arr.begin(), arr.end());
-    for (int exp = 1; max_element / exp > 0; exp *= 10) {
-        std::vector<int> output(arr.size());
-        int count[10] = {0};
-        
-        // Count occurrences
-        for (int i = 0; i < arr.size(); i++)
-            count[(arr[i] / exp) % 10]++;
-        
-        // Cumulative count
-        for (int i = 1; i < 10; i++)
-            count[i] += count[i - 1];
-        
-        // Build output array
-        for (int i = arr.size() - 1; i >= 0; i--) {
-            output[count[(arr[i] / exp) % 10] - 1] = arr[i];
-            count[(arr[i] / exp) % 10]--;
+void ParallelRadixSort(int* arr, int size, int rank, int numprocs) {
+    int global_max;
+    int local_max = *std::max_element(arr, arr + size);
+    MPI_Allreduce(&local_max, &global_max, 1, MPI_INT, MPI_MAX, MPI_COMM_WORLD);
+    
+    for (int exp = 1; global_max / exp > 0; exp *= 10) {
+        std::vector<int> temp(size, 0);
+        int bucket[10] = {0};
+
+        for (int i = 0; i < size; i++) {
+            int index = (arr[i] / exp) % 10;
+            bucket[index]++;
         }
-        
-        // Copy to original array
-        arr = output;
+
+        std::vector<int> offset(10, 0);
+        for (int i = 1; i < 10; i++) {
+            offset[i] = offset[i - 1] + bucket[i - 1];
+        }
+
+        for (int i = 0; i < size; i++) {
+            int index = (arr[i] / exp) % 10;
+            temp[offset[index]] = arr[i];
+            offset[index]++;
+        }
+
+        std::copy(temp.begin(), temp.end(), arr);
     }
 }
 
-int main(int argc, char *argv[]) {
-    int sizeOfArray; // This is an input to the code
+int main(int argc, char* argv[]) {
+    int sizeOfArray;
     int inputType;
 
     if (argc == 3) {
         sizeOfArray = atoi(argv[1]);
         inputType = atoi(argv[2]);
-        if (inputType < 0 || inputType > 3) {
-            printf("\n input type [0-3]");
-            return 0;
-        }
     } else {
-        printf("\n arguments [sizeOfArray] [input type]");
-        return 0;
+        fprintf(stderr, "Usage: %s sizeOfArray inputType\n", argv[0]);
+        exit(1);
     }
 
-    int taskid;
-    int numTasks;
-
-    MPI_Status status;
-
+    MPI_Init(&argc, &argv);
+    int rank, numprocs;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
+    
     cali::ConfigManager mgr;
     mgr.start();
-
-    CALI_MARK_BEGIN(main_cali);
-    MPI_Init(&argc, &argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &taskid);
-    MPI_Comm_size(MPI_COMM_WORLD, &numTasks);
-
-    CALI_MARK_BEGIN(data_init_runtime);
-    int* arr = GenerateArray(sizeOfArray, inputType, taskid, numTasks);
-    CALI_MARK_END(data_init_runtime);
-
-    int rank = 0;
-    while (rank < numTasks) {
-        if (rank == taskid) {
-            if (rank == 0) {
-                printf("Initializing Arrays\n");
+    
+    CALI_MARK_BEGIN("main"); //MAIN BEGIN=======================================================
+    
+    CALI_MARK_BEGIN("data_init_runtime"); //DATA INIT BEGIN=======================================================
+    int* arr = GenerateArray(sizeOfArray, inputType, rank);
+    
+    
+    // Gather initial arrays at root
+    int* all_arrays = NULL;
+    if (rank == 0) {
+        all_arrays = new int[sizeOfArray * numprocs];
+    }
+    CALI_MARK_END("data_init_runtime"); //DATA INIT END=======================================================
+    
+    CALI_MARK_BEGIN("comm"); //COMM BEGIN=======================================================
+    
+    CALI_MARK_BEGIN("comm_large"); //COMM LARGE BEGIN=======================================================
+    MPI_Gather(arr, sizeOfArray, MPI_INT, all_arrays, sizeOfArray, MPI_INT, 0, MPI_COMM_WORLD);
+    CALI_MARK_END("comm_large"); //COMM LARGE END=======================================================
+    
+    CALI_MARK_BEGIN("comm_small"); //COMM SMALL BEGIN=======================================================
+    // Print initial arrays at root
+    // if (rank == 0) {
+        // printf("Initial Arrays:\n");
+        // for (int i = 0; i < numprocs; i++) {
+            // PrintArray(all_arrays + i * sizeOfArray, sizeOfArray, i);
+        // }
+    // }
+    CALI_MARK_END("comm_small"); //COMM SMALL END=======================================================
+    
+    CALI_MARK_END("comm"); //COMM END=======================================================
+    
+    
+    
+    CALI_MARK_BEGIN("comp"); //COMP BEGIN=======================================================
+    
+    CALI_MARK_BEGIN("comp_large"); //COMP LARGE BEGIN=======================================================
+    ParallelRadixSort(arr, sizeOfArray, rank, numprocs);
+    CALI_MARK_END("comp_large"); //COMP LARGE END=======================================================
+    
+    
+    // Gather sorted arrays at root
+    CALI_MARK_BEGIN("comp_small"); //COMP SMALL BEGIN=======================================================
+    MPI_Gather(arr, sizeOfArray, MPI_INT, all_arrays, sizeOfArray, MPI_INT, 0, MPI_COMM_WORLD);
+    CALI_MARK_END("comp_small"); //COMP SMALL END=======================================================
+    
+    CALI_MARK_END("comp"); //COMP END=======================================================
+    
+    // Print sorted arrays at root
+    if (rank == 0) {
+        // printf("Sorted Arrays:\n");
+        // for (int i = 0; i < numprocs; i++) {
+            // PrintArray(all_arrays + i * sizeOfArray, sizeOfArray, i);
+        // }
+        
+        // Check correctness of the whole sorted array
+        CALI_MARK_BEGIN("correctness_check"); //CORRECTNESS BEGIN=======================================================
+        bool isSortedCorrectly = true;
+        for (int i = 0; i < numprocs; i++) {
+            if (!SortedVerification(all_arrays + i * sizeOfArray, sizeOfArray)) {
+                isSortedCorrectly = false;
+                printf("Array segment %d is not sorted correctly.\n", i);
             }
-            PrintArray(arr, sizeOfArray, taskid);
-            fflush(stdout);
         }
-        rank++;
-        MPI_Barrier(MPI_COMM_WORLD);
-    }
-
-    // Radix sort for the local arrays
-    std::vector<int> local_arr(arr, arr + sizeOfArray);
-    radixSort(local_arr);
-
-    // Make sure all arrays finish sorting 
-    MPI_Barrier(MPI_COMM_WORLD);
-
-    CALI_MARK_BEGIN(correctness_check);
-    bool arraySorted = true;
-    if (!SortedVerification(local_arr.data(), sizeOfArray)) {
-        printf("Array not sorted, processor %d\n", taskid);
-        arraySorted = false;
-    }
-    for (int i = 0; i < numTasks; ++i) {
-        if (i != 0 && i == taskid) {
-            int maxPrevArray;
-            MPI_Recv(&maxPrevArray, 1, MPI_INT, taskid - 1, 1, MPI_COMM_WORLD, &status);
-            if (maxPrevArray > local_arr[sizeOfArray - 1]) {
-                printf("Array not sorted, between processor %d and %d\n", taskid - 1, taskid);
-                arraySorted = false;
-            }
+        
+        if (isSortedCorrectly) {
+            printf("Array is sorted correctly.\n");
+        } else {
+            printf("Array is not sorted correctly.\n");
         }
-        if (i != numTasks - 1 && i == taskid) {
-            MPI_Send(&local_arr[sizeOfArray - 1], 1, MPI_INT, taskid + 1, 1, MPI_COMM_WORLD);
-        }
+        CALI_MARK_END("correctness_check"); //CORRECTNESS END=======================================================
+    
+        delete[] all_arrays; // free memory
     }
-    if (arraySorted) {
-        printf("Array Sorted!\n");
-    }
-    CALI_MARK_END(correctness_check);
-    CALI_MARK_END(main_cali);
-
-    // Print finished arrays
-    rank = 0;
-    while (rank < numTasks) {
-        if (rank == taskid) {
-            if (rank == 0) {
-                printf("Finished Arrays\n");
-            }
-            PrintArray(arr, sizeOfArray, taskid);
-            fflush(stdout);
-        }
-        rank++;
-        MPI_Barrier(MPI_COMM_WORLD);
-    }
-
+    
+    delete[] arr;
+    
+    CALI_MARK_END("main"); //MAIN END=======================================================
+    
     adiak::init(NULL);
     adiak::launchdate();    // Launch date of the job
     adiak::libraries();     // Libraries used
@@ -224,14 +211,15 @@ int main(int argc, char *argv[]) {
     adiak::value("size_of_data_type", sizeof(int)); // sizeof(datatype) of input elements in bytes (e.g., 1, 2, 4)
     adiak::value("input_size", sizeOfArray); // The number of elements in input dataset (1000)
     adiak::value("input_type", inputType); // For sorting, this would be choices: ("Sorted", "ReverseSorted", "Random", "Noise")
-    adiak::value("num_procs", numTasks); // The number of processors (MPI ranks)
+    adiak::value("num_procs", numprocs); // The number of processors (MPI ranks)
     adiak::value("scalability", "strong"); // The scalability of your algorithm. choices: ("strong", "weak")
     adiak::value("group_num", 19); // The number of your group (integer, e.g., 1, 10)
     adiak::value("implementation_source", "ai"); // Where you got the source code of your algorithm. choices: ("online", "ai", "handwritten").
-
+    
+    
     mgr.stop();
     mgr.flush();
-
     MPI_Finalize();
+    
     return 0;
 }
